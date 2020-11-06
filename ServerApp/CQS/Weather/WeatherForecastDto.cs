49196c0250cc9098c8444181229d58359135154a
zephyr <whatever@apoco.com>
2020-11-06 02:17:10 +0100

    Add project files.

---
+++ ServerApp/CQS/Weather/WeatherForecastDto.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Server
+{
+    public class WeatherForecastDto
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+
+    public class WeatherForecastFactory 
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "1Freezing", "2Bracing",// "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        internal static List<WeatherForecastDto> GetForecast(DateTime now, int daysForward)
+        {
+            var rng = new Random();
+            var dates = Enumerable.Range(1, daysForward).Select(d => now.AddDays(d));
+            var result = new List<WeatherForecastDto>();
+            foreach (var date in dates)
+            {
+                result.Add( new WeatherForecastDto {
+                    Date = date,
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                });
+            }
+            return result;
+        }
+    }
+}
---
+++ ServerApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecastDto> Get()
+        {
+            return WeatherForecastFactory.GetForecast(DateTime.Now, 5);
+        }
+    }
+}
